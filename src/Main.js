f05a8986f3186bab76b470ef39d17673ff0d0052
jamisonlewin <jamisonlewin@gmail.com>
2021-07-10 17:33:57 -0700

    setup lab 1

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HornedBeast from './HornedBeast.js';
+
+class Main extends React.Component {
+  render () {
+    return (
+      <>
+      <h1>MAIN</h1>
+      <HornedBeast />
+      </>
+    )
+  }
+}
+
+export default Main;
